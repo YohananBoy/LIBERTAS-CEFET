4d69eaea1e34df11e8103a680b4b5618fada9164
Yohanan <14911025786@cefet-rj.br>
2026-03-26 21:00:15 -0300

    feat: iniciando projeto

---
+++ src/app/_components/menu.tsx
@@ -0,0 +1,20 @@
+import Link from "@mui/material/Link";
+
+export default function Menu() {
+  return (
+    <nav className="flex flex-row gap-10">
+      <Link href="#inicio" underline="hover">
+        Início
+      </Link>
+      <Link href="#pautas" underline="hover">
+        Pautas
+      </Link>
+      <Link href="#imagens" underline="hover">
+        Imagens
+      </Link>
+      <Link href="#junte-se" underline="hover">
+        Junte-se a nós!
+      </Link>
+    </nav>
+  );
+}
