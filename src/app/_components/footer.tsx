b6c8fc7b14c3fc7f632a7553a8180dce52d0404e
Yohanan <14911025786@cefet-rj.br>
2026-03-28 21:33:49 -0300

    fix: adicionando responsividade ao footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-row items-center justify-between bg-blue-700 p-10 text-white">
+    <footer className="flex flex-col items-center justify-between gap-5 bg-blue-700 p-10 text-white md:flex-row">
       <div>
         <p>LIBERTAS CEFET</p>
         <p>&quot;Em busca da liberdade do CEFET-NF&quot;</p>
